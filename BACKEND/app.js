889797c2df1454d7660cb108241d7a590b24fd95
aarpitdas2006 <aarpitdas2006@gmail.com>
2026-08-07 01:05:54 +0530

    Prepare backend for deployment

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -92,8 +92,6 @@ app.post("/login", (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
